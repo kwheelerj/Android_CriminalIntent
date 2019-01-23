254a97b8b4d57db130496d821f3ec7f7a5a95293
kwheelerj <kwheelerj@gmail.com>
2019-01-23 00:05:51 -0500

    End of Ch 7 - connected fragment to activity

--- app/src/main/java/com/example/kwheelerj/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/kwheelerj/criminalintent/CrimeFragment.java
@@ -7,13 +7,20 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import static android.widget.CompoundButton.*;
 
 public class CrimeFragment extends Fragment {
 	/* Controller that interacts with model an view objects. */
 
 	private Crime mCrime;
 	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mCheckBox;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +30,9 @@ public class CrimeFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
 		View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
 
 		mTitleField = v.findViewById(R.id.crime_title);
 		mTitleField.addTextChangedListener(new TextWatcher() {
@@ -42,6 +51,21 @@ public class CrimeFragment extends Fragment {
 				// This space intentionally left blank.
 			}
 		});
+
+
+		mDateButton = v.findViewById(R.id.crime_date);
+		mDateButton.setText(mCrime.getDate().toString());
+		mDateButton.setEnabled(false);
+
+
+		mCheckBox = v.findViewById(R.id.crime_solved);
+		mCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				mCrime.setSolved(isChecked);
+			}
+		});
+
 
 		return v;
 	}
--- app/src/main/java/com/example/kwheelerj/criminalintent/CriminalActivity.java
+++ app/src/main/java/com/example/kwheelerj/criminalintent/CriminalActivity.java
@@ -1,5 +1,7 @@
 package com.example.kwheelerj.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,27 @@ public class CriminalActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_criminal);
+
+		FragmentManager fm = getSupportFragmentManager();
+
+		/* Use the fm to find the fragment created in the xml of the activity (framelayout) */
+		Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+		/* If fragment is already in the fm's list, the fm will return in. */
+		/* How could it already exist?  onSaveInstanceState() -> onDestroy() [rotation] */
+		/* When an activity is destroyed, its FragmentManager saves out its list of fragments. */
+		/* When the activity is recreated, the NEW FragmentManager retrieves the list (FROM WHERE)
+			(bundle?) and recreates the listed fragments to make everything as it was before. */
+
+		if (fragment == null) {
+			fragment = new CrimeFragment();
+			fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+		}
+
+		/* NOTE on the container view ID:
+		 *	1) Tells the FragmentManager where in activity view to have fragment view.
+		 *	2) Used as a unique identifier for a fragment in the FragmentManager's list.
+		 */
+
+		/* In the CrimeFragment class, the onActivityCreated(..) method is called. */
 	}
 }
