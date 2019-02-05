412f91ee21f22407e6f1ae42d58356c7b7eea5f1
kwheelerj <kwheelerj@gmail.com>
2019-02-05 00:00:36 -0500

    added View.onClick for each CrimeHolder - RecyclerView

--- app/src/main/java/com/example/kwheelerj/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/kwheelerj/criminalintent/CrimeListFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -38,7 +39,7 @@ public class CrimeListFragment extends Fragment {
 	}
 
 	/* Inner Class */
-	private class CrimeHolder extends RecyclerView.ViewHolder {
+	private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
 		private Crime mCrime;
 
@@ -47,6 +48,7 @@ public class CrimeListFragment extends Fragment {
 
 		public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
 			super(inflater.inflate(R.layout.list_item_crime, parent, false));
+			itemView.setOnClickListener(this);
 
 			mTitleTextView = itemView.findViewById(R.id.crime_title);	/* ViewHolder.itemView */
 			mDateTextView = itemView.findViewById(R.id.crime_date);
@@ -56,6 +58,11 @@ public class CrimeListFragment extends Fragment {
 			mCrime = crime;
 			mTitleTextView.setText(mCrime.getTitle());
 			mDateTextView.setText(mCrime.getDate().toString());
+		}
+
+		@Override
+		public void onClick(View view) {
+			Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
 		}
 
 	}
