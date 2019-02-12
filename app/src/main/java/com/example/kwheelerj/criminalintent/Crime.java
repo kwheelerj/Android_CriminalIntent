428e4e6d75e512370d8ded09637ea3a9b6964aa2
kwheelerj <kwheelerj@gmail.com>
2019-02-12 07:30:04 -0500

    SimpleDateFormat for readable date

--- app/src/main/java/com/example/kwheelerj/criminalintent/Crime.java
+++ app/src/main/java/com/example/kwheelerj/criminalintent/Crime.java
@@ -1,5 +1,7 @@
 package com.example.kwheelerj.criminalintent;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
@@ -7,12 +9,12 @@ public class Crime {
 
 	private UUID mId;
 	private String mTitle;
-	private Date mDate;
+	private String mDate;
 	private boolean mSolved;
 
 	public Crime() {
 		mId = UUID.randomUUID();
-		mDate = new Date();
+		mDate = (new SimpleDateFormat("EEEE, MMM dd, yyyy")).format(new Date());
 	}
 
 	public UUID getId() {
@@ -27,12 +29,12 @@ public class Crime {
 		mTitle = title;
 	}
 
-	public Date getDate() {
+	public String getDate() {
 		return mDate;
 	}
 
 	public void setDate(Date date) {
-		mDate = date;
+		mDate = (new SimpleDateFormat("EEEE, MMM dd, yyyy")).format(date);
 	}
 
 	public boolean isSolved() {
--- app/src/main/java/com/example/kwheelerj/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/kwheelerj/criminalintent/CrimeListFragment.java
@@ -60,7 +60,7 @@ public class CrimeListFragment extends Fragment {
 		public void bind(Crime crime) {
 			mCrime = crime;
 			mTitleTextView.setText(mCrime.getTitle());
-			mDateTextView.setText(mCrime.getDate().toString());
+			mDateTextView.setText(mCrime.getDate());
 			mCrimeSolved.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
 		}
 
