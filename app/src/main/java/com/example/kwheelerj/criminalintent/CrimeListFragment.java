4e970572beaac669315f3b910aa9f338d08722cf
kwheelerj <kwheelerj@gmail.com>
2019-02-12 07:11:48 -0500

    added imageview for solved crimes.  switched to constraint layout from linear layout in list item crime xml

--- app/src/main/java/com/example/kwheelerj/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/kwheelerj/criminalintent/CrimeListFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -45,6 +46,7 @@ public class CrimeListFragment extends Fragment {
 
 		private TextView mTitleTextView;
 		private TextView mDateTextView;
+		private ImageView mCrimeSolved;
 
 		public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
 			super(inflater.inflate(R.layout.list_item_crime, parent, false));
@@ -52,12 +54,14 @@ public class CrimeListFragment extends Fragment {
 
 			mTitleTextView = itemView.findViewById(R.id.crime_title);	/* ViewHolder.itemView */
 			mDateTextView = itemView.findViewById(R.id.crime_date);
+			mCrimeSolved = itemView.findViewById(R.id.crime_solved);
 		}
 
 		public void bind(Crime crime) {
 			mCrime = crime;
 			mTitleTextView.setText(mCrime.getTitle());
 			mDateTextView.setText(mCrime.getDate().toString());
+			mCrimeSolved.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
 		}
 
 		@Override
